005ac2fa5edf453be4c921744adf82e798764738
unknown <gsamikshya0@gmail.com>
2023-07-22 10:47:15 +0545

    updated

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,71 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import Navigation from "../Components/Navigation/Nav";
 import Products from "../Components/Products/Products";
 import Card from "../Components/Card";
 import Sidebar from "../Components/Sidebar/Sidebar";
-import { useState, useEffect } from "react";
 
 const HomePage = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [query, setQuery] = useState("");
+  const [products, setProducts] = useState([]);
 
-    const [selectedCategory, setSelectedCategory] = useState(null);
-    const [query, setQuery] = useState("");
-    const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((error) => {
+        console.log("Error fetching products:", error);
+        setProducts([]);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("https://fakestoreapi.com/products")
-            .then((res) => res.json())
-            .then((data) => setProducts(data))
-            .catch((error) => {
-                console.log("Error fetching products:", error);
-                setProducts([]);
-            });
-    }, []);
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
 
-    const handleInputChange = (event) => {
-        setQuery(event.target.value);
-    };
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
 
-    const filteredItems = products.filter(
-        (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
-    );
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
 
-    const handleChange = (event) => {
-        setSelectedCategory(event.target.value);
-    };
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
 
-    function filteredData(products, selected, query) {
-        let filteredProducts = products;
-
-        if (query) {
-            filteredProducts = filteredItems;
-        }
-
-        if (selected) {
-            filteredProducts = filteredProducts.filter(
-                ({ category, price, title }) =>
-                    category === selected || price === selected || title === selected
-            );
-        }
-
-        return filteredProducts.map((product) => (
-            <Card
-                key={product.id}
-                product={product}
-            />
-        ));
+    if (query) {
+      filteredProducts = filteredItems;
     }
 
-    const result = filteredData(products, selectedCategory, query);
-    return (
-        <>
-            <Sidebar handleChange={handleChange} />
-            <Navigation
-                query={query}
-                handleInputChange={handleInputChange}
-                handleSearch={() => filteredData(products, selectedCategory, query)}
-            />
-            <Products result={result} /></>
-    )
-}
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, price, title }) =>
+          category === selected || price === selected || title === selected
+      );
+    }
 
-export default HomePage
+    return filteredProducts.map((product) => (
+      <Card key={product.id} product={product} />
+    ));
+  }
+
+  const result = filteredData(products, selectedCategory, query);
+  return (
+    <>
+      <Sidebar handleChange={handleChange} />
+      <Navigation
+        query={query}
+        handleInputChange={handleInputChange}
+        handleSearch={() => filteredData(products, selectedCategory, query)}
+      />
+      {!products.length < 1 ? (
+        <Products result={result} />
+      ) : (
+        <h2 style={{ textAlign: "center", margin: "40px" }}>Loarding...</h2>
+      )}
+    </>
+  );
+};
+
+export default HomePage;
